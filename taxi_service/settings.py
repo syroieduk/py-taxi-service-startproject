35e88a08c61548b29be766901915bc1f5bedc9e4
Yurii Syroied <syroiedyur@gmail.com>
2026-09-23 08:30:41 +0300

    solution_4

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -121,10 +121,6 @@ STATIC_URL = "static/"
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-EMAIL_BACKEND = {
-    "default": {
-        "BACKEND": "django.core.mail.backends.console.EmailBackend",
-    },
-}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 AUTH_USER_MODEL = 'taxi.Driver'
